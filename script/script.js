6044a1f4c6e106aea9eb8bca805b051f574627d4
Nick Hughes <nickhughes17@hotmail.com>
2022-06-09 21:27:14 -0700

    scripting changes

--- script/script.js
+++ script/script.js
@@ -7,6 +7,12 @@ var closeButton2 = document.getElementById("less-2");
 var openButton3 = document.getElementById("dots-3");
 var moreText3 = document.getElementById("more-3");
 var closeButton3 = document.getElementById("less-3");
+var openButton4 = document.getElementById("dots-4");
+var moreText4 = document.getElementById("more-4");
+var closeButton4 = document.getElementById("less-4");
+var openButton5 = document.getElementById("dots-5");
+var moreText5 = document.getElementById("more-5");
+var closeButton5 = document.getElementById("less-5");
 
 
 openButton1.addEventListener("click", toggleOpen);
@@ -15,6 +21,10 @@ openButton2.addEventListener("click", toggleOpen);
 closeButton2.addEventListener("click", toggleClose);
 openButton3.addEventListener("click", toggleOpen);
 closeButton3.addEventListener("click", toggleClose);
+openButton4.addEventListener("click", toggleOpen);
+closeButton4.addEventListener("click", toggleClose);
+openButton5.addEventListener("click", toggleOpen);
+closeButton5.addEventListener("click", toggleClose);
 
 function toggleOpen(event) {
     event.target.style.display = "none";
