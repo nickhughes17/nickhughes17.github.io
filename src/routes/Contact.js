e4c35dbcfabde2d0cac47dcd0b3da66cde85382f
Nick Hughes <nickhughes17@hotmail.com>
2022-08-26 14:39:38 -0700

    change

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -4,7 +4,7 @@ import '../styles/Contact.css';
 const Contact = () => {
   return (
     <div className='contact-container'>
-        <div className="contact">Reach out. <br />
+        <div className="contact">Reach out.<br />
             <a href="mailto:nickhughes1796@gmail.com">NickHughes1796@gmail.com</a>
         </div>
     </div>
